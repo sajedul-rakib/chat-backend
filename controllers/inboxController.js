cd45b744e204c835875e9948ce82154548ecc359
Sajedul Islam Rakib <rakib.sajedul@gmail.com>
2025-04-13 03:26:51 +0600

    User online/offline methodology integrated successfully

--- controllers/inboxController.js
+++ controllers/inboxController.js
@@ -1,6 +1,9 @@
 //user seassion : check the user now active in her conversation
 const userSession = {};
-const userOnline = {};
+const userOnline = [];
+
+//external package
+const mongoose = require("mongoose");
 
 //internal package
 const Conversation = require("../models/Conversation");
@@ -11,19 +14,25 @@ const { sendNotification } = require("./notificationController");
 
 //check user now online or offline
 global.io.on("connection", (socket) => {
-  socket.on("user_online", ({ userId, isOnline }) => {
-    userOnline[userId] = isOnline;
+  socket.on("user_online", ({ userId }) => {
+    let index = userOnline.indexOf(userId);
+    if (index < 0) {
+      userOnline.push(userId);
+    }
     global.io.emit("online_user", userOnline);
   });
 
   socket.on("user_offline", ({ userId }) => {
-    console.log(userId);
-    delete userOnline[userId];
+    let indexItem = userOnline.indexOf(userId);
+    if (indexItem != -1) {
+      userOnline.splice(indexItem, 1);
+    }
+
     global.io.emit("online_user", userOnline);
   });
 });
 
-//for check the friend on sceen either send a notification to friend his take attenstion
+//for check the friend on screen either send a notification to friend his take attenstion
 global.io.on("connection", (socket) => {
   socket.on("join_chat", ({ conversationId, userId }) => {
     userSession[userId] = conversationId;
@@ -98,8 +107,12 @@ async function createConversation(req, res) {
     });
 
     if (findTheUserExistWithTheUser && findTheUserExistWithTheUser.length > 0) {
-      res.status(200).json({
-        msg: "The user already add with you",
+      res.status(406).json({
+        errors: {
+          common: {
+            msg: "This user already add with you",
+          },
+        },
       });
     } else {
       const createConversation = new Conversation({
@@ -108,6 +121,17 @@ async function createConversation(req, res) {
       });
       const result = await createConversation.save();
 
+      //friend user detail
+      const friend = await User.findById(req.body._id);
+
+      //send notification
+      if (friend.fcmToken !== "") {
+        sendNotification(
+          "A new friend added",
+          `${req.fullName} add with you, now you can send and recieve message`,
+          friend.fcmToken
+        );
+      }
       await result.populate(["owner", "participant"]);
 
       res.status(200).json({
@@ -203,27 +227,47 @@ async function searchUser(req, res) {
   const keyword = req.body.key;
 
   try {
-    if (keyword === "" || keyword === undefined) {
-      res.json({ msg: "You must provide some text to find user" });
-    } else {
-      const name_search_regex = new RegExp(escapeKey(keyword), "i");
-      const email_search_regex = new RegExp(
-        "^" + escapeKey(keyword) + "$",
-        "i"
-      );
-      const result = await User.find({
-        $or: [{ fullName: name_search_regex }, { email: email_search_regex }],
-      });
-
-      res.status(200).json({
-        data: result,
-      });
+    if (!keyword || keyword.trim() === "") {
+      return res.json({ msg: "You must provide some text to find user" });
     }
+
+    const name_search_regex = new RegExp(escapeKey(keyword), "i");
+    const email_search_regex = new RegExp("^" + escapeKey(keyword) + "$", "i");
+
+    // 1. Find your friend list (conversations where you're owner or participant)
+    const conversations = await Conversation.find({
+      $or: [{ owner: req.userId }, { participant: req.userId }],
+    });
+
+    // 2. Extract friend user IDs from conversations
+    const friendIds = conversations.map((conv) => {
+      if (conv.owner.toString() === req.userId.toString()) {
+        return conv.participant.toString();
+      } else {
+        return conv.owner.toString();
+      }
+    });
+
+    // 3. Exclude yourself and your friends
+    const result = await User.find({
+      _id: {
+        $nin: [
+          req.userId,
+          ...friendIds.map((id) => new mongoose.Types.ObjectId(id)),
+        ],
+      },
+      $or: [{ fullName: name_search_regex }, { email: email_search_regex }],
+    });
+
+    res.status(200).json({
+      data: result,
+    });
   } catch (err) {
+    console.error(err);
     res.status(500).json({
       errors: {
         common: {
-          msg: "There was an server side error",
+          msg: "There was a server-side error",
         },
       },
     });

9176c288ccb307a0c4aef5cd4f812a961ca7ec12
Sajedul Islam Rakib <rakib.sajedul@gmail.com>
2025-07-18 19:35:31 +0600

    Error Debuging at user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,19 +11,10 @@ const User = require("../models/User");
 async function singUpController(req, res) {
   try {
     const { fullName, email, password, gender } = req.body;
-    // const profilePic =
-    //   req.files.length > 0
-    //     ? `${process.env.APP_URL}/public/uploads/avatars/${req.files[0].filename}`
-    //     : "";
-
-    let profilePic = "";
-
-    if (req.files) {
-      profilePic =
-        req.files.length > 0
-          ? `/public/uploads/avatars/${req.files[0].filename}`
-          : "";
-    }
+    const profilePic =
+      req.files.length > 0
+        ? `${process.env.APP_URL}/public/uploads/avatars/${req.files[0].filename}`
+        : "";
 
     //hashing password
     const hashPassword = await bcrypt.hash(password, 10);
